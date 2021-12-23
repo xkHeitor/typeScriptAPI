c00105a61f559793a24c524d8f1c701b8eeef271
xkHeitor <heitorcarvalhorod@gmail.com>
2021-12-23 13:40:59 -0300

    improvements and new user testing

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,13 +7,17 @@ export interface User {
 	password: string;
 }
 
+export enum CUSTOM_VALIDATION {
+	DUPLICATED = 'DUPLICATED',
+}
+
 interface UserModel extends Omit<User, '_id'>, Document {}
 
 const schema = new mongoose.Schema(
 	{
 		name: { type: String, required: true },
 		password: { type: String, required: true },
-		email: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
 	}, {
 		toJSON: {
 			transform: (_, ret): void => {
@@ -24,5 +28,10 @@ const schema = new mongoose.Schema(
 		}
 	}
 );
+
+schema.path('email').validate(async (email: string) => {
+	const emailCount = await mongoose.models.User.countDocuments({ email });
+	return !emailCount;
+}, 'already exists in the database.', CUSTOM_VALIDATION.DUPLICATED);
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -7,14 +7,33 @@ describe('Users functional tests', () => {
 	});
 
 	describe('When creating a new user', () => {
-		it ('should successfully create a new user', async () => {
-			const newUser = { 
-				name: 'John Doe', email: 'john@mail.com', password: '1234'
-			};
-			
-			const response = await global.testRequest.post('/users').send(newUser); 
+
+		const newUserDefault = { 
+			name: 'John Doe', email: 'john@mail.com', password: '1234'
+		};
+
+		it ('Should successfully create a new user', async () => {
+			const response = await global.testRequest.post('/users').send(newUserDefault); 
 			expect(response.status).toBe(201);
-			expect(response.body).toEqual(expect.objectContaining(newUser))
+			expect(response.body).toEqual(expect.objectContaining(newUserDefault))
+		});
+
+		it ('Should return 400 when there is a validation error', async () => {
+			const newUser = { email: 'john@mail.com', password: '1234' };
+			const response = await global.testRequest.post('/users').send(newUser);
+			expect(response.status).toBe(422);
+			expect(response.body).toEqual({
+				code: 422, error: 'User validation failed: name: Path `name` is required.'
+			});
+		});
+
+		it ('Should return 409 when the email already exists', async () => {
+			await global.testRequest.post('/users').send(newUserDefault);
+			const response = await global.testRequest.post('/users').send(newUserDefault);
+			expect(response.status).toBe(409);
+			expect(response.body).toEqual({
+				code: 409, error: 'User validation failed: email: already exists in the database.'
+			});
 		});
 	});
 });
